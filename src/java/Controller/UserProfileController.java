13707759f2be04220dd80d75b183bc3c935c82fc
baovu8102 <baomvhe161593@fpt.edu.vn>
2023-09-20 16:10:12 +0700

    userprofilecontroller update

--- src/java/Controller/UserProfileController.java
+++ src/java/Controller/UserProfileController.java
@@ -8,6 +8,7 @@ package Controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author Mai Vu Bao
  */
+@WebServlet(name="UserProfileController", urlPatterns={"/UserProfile"})
 public class UserProfileController extends HttpServlet {
    
     /** 
@@ -63,7 +65,6 @@ public class UserProfileController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         processRequest(request, response);
