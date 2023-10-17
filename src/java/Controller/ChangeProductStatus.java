93eae3868301fde04aefe638c7eb1ce5b6e676fb
LAPTOP-GIBR179P\Acer <hoangnhhe172848@fpt.edu.vn>
2023-10-17 08:16:05 +0700

    login va changestt

--- src/java/Controller/ChangeProductStatus.java
+++ src/java/Controller/ChangeProductStatus.java
@@ -5,28 +5,21 @@
 
 package Controller;
 
-import DAO.DAO;
 import DAO.UserDAO;
-import Model.Account;
-import Model.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Acer
  */
-@WebServlet(name="LoginController", urlPatterns={"/login"})
-public class LoginController extends HttpServlet {
+@WebServlet(name="ChangeProductStatus", urlPatterns={"/productstatus"})
+public class ChangeProductStatus extends HttpServlet {
    
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -43,10 +36,10 @@ public class LoginController extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Login</title>");  
+            out.println("<title>Servlet ChangeProductStatus</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Login at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet ChangeProductStatus at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -63,7 +56,7 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.sendRedirect("login.jsp");
+        processRequest(request, response);
     } 
 
     /** 
@@ -76,43 +69,19 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        String e = request.getParameter("email");
-        String p = request.getParameter("password");
-        String r=request.getParameter("rem");
-        DAO ad = new DAO();
-        UserDAO ud= new UserDAO();
-        Account c = ad.getAccount(e, p);
-        int userID = ud.cusAccountExist(e, p);
-        try {
-                   if (c == null) {
-            request.setAttribute("mess", "Wrong user name or password!");
-            String er = "username: " + e + " and password: " + p + " don't exsited!";
-            request.setAttribute("error", er);
-            request.getRequestDispatcher("login.jsp").forward(request, response);
-        } else {
-            HttpSession session = request.getSession(true);
-            User user = (new UserDAO()).getUser(userID);
-            session.setAttribute("user", user);
-            session.setAttribute("acc", c);
-            session.setMaxInactiveInterval(1800);
-Cookie cookie = new Cookie("rem", r);
-if (r == null || r.isEmpty()) {
-    cookie.setMaxAge(0);
-} else {
-    cookie.setMaxAge(24 * 60 * 60);  // 1 day in seconds
-}
-response.addCookie(cookie);
-if(c.getRoleID() == 2){
-    response.sendRedirect("statistic");
-}if(c.getRoleID() == 1){
-    response.sendRedirect("admin");
-}else{
-    response.sendRedirect("home");
-}
-        }
-        } catch (Exception ex) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception", e);
-        }
+        int currentStatus = Integer.parseInt(request.getParameter("status"));
+    int id = Integer.parseInt(request.getParameter("id"));
+
+
+    // Kiểm tra và xử lý dữ liệu
+      int newStatus = (currentStatus == 0) ? 1 : 0;
+        // Gọi phương thức xử lý dữ liệu
+          UserDAO dao = new UserDAO();
+      
+        dao.ChangeSettingStatusProduct(id, newStatus);
+         
+        // Chuyển hướng trang sau khi xử lý
+        response.sendRedirect("manage");
     }
 
     /** 
@@ -123,20 +92,5 @@ if(c.getRoleID() == 2){
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-    private boolean checkPass(String pass, String confpass) {
-        if (pass.length() >= 6) {
-            if (pass.equals(confpass)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private boolean checkUserName(String userName){
-        if(userName.length() == 0){
-            System.out.println("You must input email");
-        }else{
-            return true;
-        }
-     return false;
-    }
+
 }
