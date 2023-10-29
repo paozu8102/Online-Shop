20a9e72f2198de6e8baafdd442ca4adace1eef3e
“[firstname <minhhche171312@fpt.edu.vn>
2023-10-29 22:38:48 +0700

    edit product func

--- src/java/DAO/ProductDAO.java
+++ src/java/DAO/ProductDAO.java
@@ -9,6 +9,7 @@ import Model.Category;
 import Model.Product;
 import Model.Setting;
 import java.math.BigDecimal;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -1504,6 +1505,123 @@ public class ProductDAO extends DBContext {
 
         return list;
     }
+  public Product getProduct(String id) {
+
+        String sql = "WITH RankedImages AS (\n"
+                + "    SELECT\n"
+                + "        I.ObjectID AS ProductID,\n"
+                + "        I.ImageUrl,\n"
+                + "        ROW_NUMBER() OVER (PARTITION BY I.ObjectID ORDER BY I.ImageID) AS ImageRank\n"
+                + "    FROM\n"
+                + "        [dbo].[Image] AS I\n"
+                + "    WHERE\n"
+                + "        I.TypeID = 1  -- Assuming TypeID 1 corresponds to products\n"
+                + "),\n"
+                + "RankedCategories AS (\n"
+                + "    SELECT\n"
+                + "        PC.ProductID,\n"
+                + "        C.CategoryID,\n"
+                + "        C.CategoryName,\n"
+                + "        ROW_NUMBER() OVER (PARTITION BY PC.ProductID ORDER BY C.CategoryID) AS CategoryRank\n"
+                + "    FROM\n"
+                + "        ProductCategory AS PC\n"
+                + "    JOIN\n"
+                + "        Category AS C ON PC.CategoryID = C.CategoryID\n"
+                + ")\n"
+                + "\n"
+                + "SELECT Top 1\n"
+                + "    P.ProductID,\n"
+                + "    P.ProductName,\n"
+                + "    P.Price,\n"
+                + "    P.[Description] AS ProductDescription,\n"
+                + "    P.Height,\n"
+                + "    P.Width,\n"
+                + "    P.Quantity,\n"
+                + "    P.[View],\n"
+                + "    P.Discount,\n"
+                + "    P.UserID,\n"
+                + "    R.ImageUrl AS ProductImage,\n"
+                + "    RC.CategoryID AS FirstCategoryID,\n"
+                + "    RC.CategoryName AS FirstCategoryName,\n"
+                + "    P.Status AS Status\n"
+                + "FROM\n"
+                + "    Product AS P\n"
+                + "LEFT JOIN\n"
+                + "    RankedImages AS R ON P.ProductID = R.ProductID\n"
+                + "LEFT JOIN\n"
+                + "    RankedCategories AS RC ON P.ProductID = RC.ProductID\n"
+                + "INNER JOIN\n"
+                + "    ProductCategory AS PC ON P.ProductID = PC.ProductID\n"
+                + "WHERE\n"
+                + "    R.ImageRank = 1\n"
+                + "    AND RC.CategoryRank = 1\n"
+                + "    AND PC.ProductID = ?\n"
+                + "ORDER BY P.ProductID";
+
+        try {
+            PreparedStatement st = getConnection().prepareStatement(sql);
+
+            st.setString(1, id);
+            ResultSet rs = st.executeQuery();
+
+          while (rs.next()) {
+    return new Product(
+        rs.getInt("ProductID"),
+        rs.getString("ProductName"),
+        rs.getDouble("Price"),
+        rs.getString("ProductDescription"),
+        rs.getDouble("Height"),
+        rs.getDouble("Width"),
+        rs.getInt("Quantity"),
+        rs.getInt("View"),
+        rs.getDouble("Discount"),
+        rs.getInt("UserID"),
+        rs.getString("ProductImage"),
+        rs.getInt("FirstCategoryID"),
+        rs.getInt("Status")
+    );
+}
+
+        } catch (SQLException e) {
+            // Handle SQL exception
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "SQL Exception", e);
+        } catch (Exception e) {
+            // Handle other exceptions
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception", e);
+        }
+
+        return null;
+    }
+  
+  
+    public void updateProduct(String id, String name, String price, String description, String height, String width, String quantity, String discount, String status) {
+    try {
+        // Get your database connection
+        String sql = "UPDATE Product SET ProductName=?, Price=?, Description=?, Height=?, Width=?, Quantity=?, Discount=?, Status=? WHERE ProductID=?";
+      PreparedStatement statement = getConnection().prepareStatement(sql);
+
+        statement.setString(1, name);
+        statement.setString(2, price);
+        statement.setString(3, description);
+        statement.setString(4, height);
+        statement.setString(5, width);
+        statement.setString(6, quantity);
+        statement.setString(7, discount);
+        statement.setString(8, status);
+        statement.setString(9, id);
+
+        statement.executeUpdate();
+
+        // Close resources
+
+    } catch (SQLException e) {
+        // Handle SQL exception
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, "SQL Exception", e);
+    } catch (Exception e) {
+        // Handle other exceptions
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception", e);
+    }
+}
 
     public Product getProductByID(String id) {
 
@@ -1542,7 +1660,7 @@ public class ProductDAO extends DBContext {
                 + "    P.UserID,\n"
                 + "    R.ImageUrl AS ProductImage,\n"
                 + "    RC.CategoryID AS FirstCategoryID,\n"
-                + "    RC.CategoryName AS FirstCategoryName\n"
+                + "    RC.CategoryName AS FirstCategoryName,\n"
                 + "FROM\n"
                 + "    Product AS P\n"
                 + "LEFT JOIN\n"
